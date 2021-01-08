2ac770c50cf1d340e7523e5b150fdabeb3103325
DarwinCocom14 <60950028+DarwinEligioCocomMis@users.noreply.github.com>
2021-01-07 21:15:14 -0600

    Add files via upload

---
+++ mensaje.java
@@ -0,0 +1,25 @@
+
+public class mensaje {
+        
+
+    public static void main(String[] args) {
+    	double suma; int i; int cont = 0;	
+    	
+    	do
+    		{
+    			double num1 = Math.random() * 10; double num2 = Math.random() * 10;
+    			suma = num1 + num2;
+    			
+    			System.out.println("La suma total es: " + suma);
+    				if(suma > 9)
+    		{
+    			System.out.println("El Número Alcanza la Decena.");
+    			System.out.println();
+    		}else{ System.out.println("El Número solo Alcanza la Unidad.");
+    		System.out.println();}
+    		
+    			cont = cont + 1;	
+    		}while (cont < 5);
+    		    	
+    }
+}
